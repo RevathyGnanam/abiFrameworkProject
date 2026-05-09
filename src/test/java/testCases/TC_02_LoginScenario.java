d35de290eddaf349f86cd7d5900c2fb418857774
RevathyGnanam <revathygovindasamy241193@gmail.com>
2026-05-09 21:23:54 +0530

    Update TC_02_LoginScenario.java

--- src/test/java/testCases/TC_02_LoginScenario.java
+++ src/test/java/testCases/TC_02_LoginScenario.java
@@ -20,6 +20,7 @@ public class TC_02_LoginScenario extends BaseClass {
 		loginpage.enterEmailId(prp.getProperty("username"));
 		loginpage.enterpassword(prp.getProperty("password"));
 		loginpage.clickLoginButton();
+		System.out.println("Revathy Changes");
 		DemoHomePage hp = new DemoHomePage(driver);
 		Assert.assertTrue(hp.verifyLogoutButton());
 		
